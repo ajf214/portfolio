18bfccf45b21edff1c8f08fe308b167b04d9ab0a
ajf214 <alex.j.frankel@gmail.com>
2019-04-14 20:28:51 -0700

    halfway through getting deploy to work..

--- src/views/PortfolioHome.vue
+++ src/views/PortfolioHome.vue
@@ -12,9 +12,12 @@
     <div class="projects">
       <div class="project grid" v-for="(project, index) in projects" :key="index">
         <div class="project-copy">
-          <router-link :to="`/projects/${project.name}`">
+          <!-- <router-link :to="`/projects/${project.name}`">
             <h3>{{project.displayName}}</h3>
-          </router-link>
+          </router-link> -->
+          <a :href="project.link" target="_blank">
+            <h3>{{project.displayName}}</h3> 
+          </a>
           <p class="description">{{project.description}}</p>
         </div>
         <div class="graphic" :id="project.name" :class="{short: project.name === 'project-watt'}">
@@ -38,6 +41,7 @@ export default {
           description: `The world’s largest enterprises were having challenges scaling their consumption 
           of Microsoft Azure because new application environments took too long to set up. Blueprints allows 
           cloud architects to set up an environment in a few clicks.`,
+          link: 'https://www.youtube.com/watch?v=cQ9D-d6KkMY'
         },
         {
           name: "change-a-view",
@@ -45,7 +49,8 @@ export default {
           description: `Reddit’s r/changemyview subreddit — with over 700,000 subscribers, was hitting the 
           limits of how it could innovate in improving discussions. Widely regarded as one of the most unique 
           communities on reddit, the team decided that the community needed to exist on it’s own site, where 
-          they could expand what would be possible with discussion on the internet.`
+          they could expand what would be possible with discussion on the internet.`,
+          link: 'https://www.changeaview.com/'
         },
         {
           name: "project-watt",
@@ -53,7 +58,8 @@ export default {
           description: `My master’s thesis at SVA IxD, For a year, I explored how we can better have complex, 
           nuanced discussions on the internet— motivated by the aftermath of the 2016 elections. projectWATT 
           is a publication which features OPs from the most popular threads on the r/changemyview subreddit 
-          who summarize their CMV experience so it can be shared beyond reddit.`
+          who summarize their CMV experience so it can be shared beyond reddit.`,
+          link: 'https://projectwatt.com'
         }
       ],
       bio: `I’m a product manager, product designer, and dog lover. I spend most of my time at 
